d05a5a1718dfeaced920022d922367db94666e04
optimus775 <mail@normus.ru>
2026-01-02 14:35:32 +0100

    версия 3.17.1 без изменений

--- muon-app/src/main/java/muon/app/util/Constants.java
+++ muon-app/src/main/java/muon/app/util/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
     public static final String BASE_UPDATE_URL = "https://devlinx9.github.io/muon-ssh";
     public static final String API_UPDATE_URL = "https://api.github.com/repos/devlinx9/muon-ssh/releases/latest";
     public static final String REPOSITORY_URL = BASE_URL + "/muon-ssh";
-    public static final String APPLICATION_VERSION = "3.0.0";
+    public static final String APPLICATION_VERSION = "3.17.1";
     public static final String APPLICATION_NAME = "Muon SSH";
     public static final String SESSION_DB_FILE = "session-store.json";
     public static final String CONFIG_DB_FILE = "settings.json";
@@ -21,7 +21,7 @@ public class Constants {
     public static final String PINNED_LOGS = "pinned-logs.json";
     public static final String TRANSFER_HOSTS = "transfer-hosts.json";
     public static final String BOOKMARKS_FILE = "bookmarks.json";
-    public static final String TOOLTIP_FORMAT="<html>%s <span style='font-size:smaller; color:gray;'>(%s)</span></html>";
+    public static final String TOOLTIP_FORMAT = "<html>%s <span style='font-size:smaller; color:gray;'>(%s)</span></html>";
 
     public static final String APP_INSTANCE_ID = UUID.randomUUID().toString();
 
